494a56c1b4a4ddc50d585894a6f666141373a6d2
valen <vcorrea@fi.uba.ar>
2022-12-06 16:31:39 -0300

    delete target

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,21 +1,16 @@
 use std::env;
 
-/*mod ux;
-mod sokoban_service;
-mod file_service;
-mod command_service;
-mod utils;
-mod movement_service;
-mod map_service;*/
 mod api;
+mod front;
 
 use crate::api::sokoban_service::{play, SokobanError};
-
+use crate::front::window::run_app;
 
 fn main() -> Result<(), SokobanError> { // todo generalizar error
     let map: Vec<String> = env::args().collect();
 
     match play(&map[1]) { // todo mencionar como ventaja del lenguaje
+        //Ok(_) => run_app(),
         Ok(_) => Ok(()),
         Err(err) => Err(err)
     }
